c43bea90b3c63e0ce6c6bf3d83d1c86988502d43
ralphlevan@gmail.com <ralphlevan@gmail.com@3d8c3fd8-b64c-11dd-bd25-4dde899625b4>
2015-01-05 01:15:15 +0000

    Starting the process

---
+++ src/java/ORG/oclc/os/SRW/OpenSearchRecordIterator.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ORG.oclc.os.SRW;
+
+import gov.loc.www.zing.srw.ExtraDataType;
+
+/**
+ *
+ * @author Ralph
+ */
+class OpenSearchRecordIterator implements RecordIterator {
+
+    public OpenSearchRecordIterator(long index, int numRecs, String schemaId, ExtraDataType edt) {
+    }
+
+    public void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Record nextRecord() throws SRWDiagnostic {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean hasNext() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Object next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
