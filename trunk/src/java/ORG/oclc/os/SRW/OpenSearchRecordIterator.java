f731e4f806e824987556eb44cded7df29660be04
ralphlevan@gmail.com <ralphlevan@gmail.com@3d8c3fd8-b64c-11dd-bd25-4dde899625b4>
2015-02-04 03:02:01 +0000

    First pass at turning an item into a Record

--- trunk/src/java/ORG/oclc/os/SRW/OpenSearchRecordIterator.java
+++ trunk/src/java/ORG/oclc/os/SRW/OpenSearchRecordIterator.java
@@ -6,34 +6,81 @@
 package ORG.oclc.os.SRW;
 
 import gov.loc.www.zing.srw.ExtraDataType;
+import java.util.AbstractList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+import java.util.RandomAccess;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  *
  * @author Ralph
  */
 class OpenSearchRecordIterator implements RecordIterator {
+    private final ListIterator<Node> items;
 
-    public OpenSearchRecordIterator(long index, int numRecs, String schemaId, ExtraDataType edt) {
+    public OpenSearchRecordIterator(long index, int numRecs, String schemaId,
+            ExtraDataType edt, Element channel) {
+        items = asList(channel.getElementsByTagNameNS("*", "item")).listIterator();
     }
 
+    @Override
     public void close() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
     public Record nextRecord() throws SRWDiagnostic {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Record record = new Record("", "rss");
+        Element item=(Element) items.next();
+        record.setIdentifier(item.getElementsByTagName("guid").item(0).getTextContent());
+        return record;
     }
 
+    @Override
     public boolean hasNext() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return items.hasNext();
     }
 
+    @Override
     public Object next() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            return nextRecord();
+        } catch (SRWDiagnostic ex) {
+            throw new NoSuchElementException();
+        }
     }
 
+    @Override
     public void remove() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    public static List<Node> asList(NodeList n) {
+        return n.getLength() == 0
+                ? Collections.<Node>emptyList() : new NodeListWrapper(n);
+    }
+
+    static final class NodeListWrapper extends AbstractList<Node>
+            implements RandomAccess {
+
+        private final NodeList list;
+
+        NodeListWrapper(NodeList l) {
+            list = l;
+        }
+
+        @Override
+        public Node get(int index) {
+            return list.item(index);
+        }
+
+        @Override
+        public int size() {
+            return list.getLength();
+        }
+    }
 }
